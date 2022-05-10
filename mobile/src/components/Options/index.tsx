86de276db9ba54d130c190fc83ed21578552aebf
x0446891 <fabriciosouza@santos.sp.gov.br>
2022-05-10 14:07:18 -0300

    aplicaçao mobile finalizada

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Option } from '../Option'
+import { Option } from '../Option';
+import { FeedbackType } from '../Widget';
 
 import { feedbackTypes } from '../../utils/feedbackTypes'
 import { styles } from './styles';
 
-export function Options() {
+interface Props {
+    onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export function Options({ onFeedbackTypeChanged }: Props) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
@@ -21,7 +26,7 @@ export function Options() {
                             key={key}
                             title={value.title}
                             image={value.image}
-                            />
+                            onPress={() => onFeedbackTypeChanged(key as FeedbackType)} />
                     ))
                 }
             </View>
